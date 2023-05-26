c1a8a15004945de2f7950e3827073da4ec24dbd4
MaateusMDS <maateusmania@gmail.com>
2023-05-26 09:10:08 -0300

    starting project

---
+++ src/main/java/com/fome/FomeApplication.java
@@ -0,0 +1,13 @@
+package com.fome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FomeApplication.class, args);
+	}
+
+}
